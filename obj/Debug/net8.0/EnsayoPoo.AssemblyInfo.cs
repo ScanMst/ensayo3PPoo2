c12fa1fc4cda13a797ec3f854972f2d3d57ef59c
Scarew.net <134906636+ScanMst@users.noreply.github.com>
2024-07-10 02:43:02 -0600

    agregando 2 desde ramaprueba

--- obj/Debug/net8.0/EnsayoPoo.AssemblyInfo.cs
+++ obj/Debug/net8.0/EnsayoPoo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnsayoPoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c2ec396c5d2455a37f6431ca18be0cff4caacc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9c082cb9bdc8a10411ce3d2436946b95d52d330")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnsayoPoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnsayoPoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
